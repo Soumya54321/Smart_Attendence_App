49052e837106a33e7889a2d6a25f301254e8aa0f
Srinjan Ghosh <srinjanghosh11@gmail.com>
2020-01-23 18:39:03 +0530

    2nd try

--- android/Attendance/app/src/main/java/com/example/attendance/Api.java
+++ android/Attendance/app/src/main/java/com/example/attendance/Api.java
@@ -3,12 +3,20 @@ package com.example.attendance;
 import com.example.attendance.model.User;
 
 
+import okhttp3.MultipartBody;
+import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.Multipart;
 import retrofit2.http.POST;
+import retrofit2.http.Part;
 
 public interface Api {
 
     @POST("login")
     Call<User> login(@Body User user);
+
+    @Multipart
+    @POST("/upload")
+    Call<ResponseBody> upload(@Part MultipartBody.Part file);
 }
--- android/Attendance/app/src/main/java/com/example/attendance/MainActivity.java
+++ android/Attendance/app/src/main/java/com/example/attendance/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.attendance;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -7,18 +8,28 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.FileUtils;
 import android.provider.MediaStore;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 import android.widget.VideoView;
 
 import com.example.attendance.model.Post;
 import com.example.attendance.model.User;
 
+import java.io.File;
 import java.util.List;
 
+import okhttp3.MediaType;
+import okhttp3.MultipartBody;
+import okhttp3.RequestBody;
+import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -30,8 +41,10 @@ public class MainActivity extends AppCompatActivity {
     private boolean loggedIn = false;
 
     TextView textView;
-    Button logout_btn, btn_pic;
+    Button btn_pic, upload_btn;
     VideoView videoView;
+
+    Uri videoUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,9 +52,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         textView = findViewById(R.id.text_view);
-        logout_btn = findViewById(R.id.logout_btn);
+        upload_btn = findViewById(R.id.upload_btn);
         btn_pic = findViewById(R.id.btn_pic);
         videoView = findViewById(R.id.videoView);
+        videoView.setVisibility(View.INVISIBLE);
 
         if (!new SharedPrefs().getLoggedInStatus(MainActivity.this)) {
             startActivity(new Intent(this, LoginActivity.class));
@@ -50,18 +64,17 @@ public class MainActivity extends AppCompatActivity {
         String displayString = new SharedPrefs().getEmail(MainActivity.this);
         textView.setText("Hello " + displayString);
 
-        logout_btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                new SharedPrefs().setLoggedIn(MainActivity.this);
-                startActivity(new Intent(MainActivity.this, LoginActivity.class));
-            }
-        });
-
         btn_pic.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 takePicture();
+            }
+        });
+
+        upload_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                uploadFile();
             }
         });
 
@@ -104,6 +117,59 @@ public class MainActivity extends AppCompatActivity {
  */
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.logout:
+                logout();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void logout(){
+        new SharedPrefs().setLoggedIn(MainActivity.this);
+        startActivity(new Intent(MainActivity.this, LoginActivity.class));
+    }
+
+    private void uploadFile() {
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("192.168.29.12:5000")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        Api api = retrofit.create(Api.class);
+
+        File file = new File(videoUri.getPath());
+
+        RequestBody requestFile = RequestBody.create(MediaType.parse("multipart/form-data"), file);
+
+        MultipartBody.Part partFile = MultipartBody.Part.createFormData("upload", file.getName(), requestFile);
+
+
+        Call<ResponseBody> call = api.upload(partFile);
+
+        call.enqueue(new Callback<ResponseBody>() {
+            @Override
+            public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+                if(!response.isSuccessful()){
+                    Toast.makeText(MainActivity.this, "Code: " + response.code() , Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<ResponseBody> call, Throwable t) {
+                Toast.makeText(MainActivity.this, t.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
     static final int REQUEST_VIDEO_CAPTURE = 1;
     private void takePicture() {
         Intent takeVideoIntent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
@@ -115,10 +181,13 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         if (requestCode == REQUEST_VIDEO_CAPTURE && resultCode == RESULT_OK) {
-            Uri videoUri = data.getData();
+            videoUri = data.getData();
             System.out.println(videoUri);
+            videoView.setVisibility(View.VISIBLE);
             videoView.setVideoURI(videoUri);
             videoView.start();
         }
     }
+
+
 }
