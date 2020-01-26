ab0d61fce4acefdfdd82ccf6a5c4abb1c93aecb5
Srinjan Ghosh <srinjanghosh11@gmail.com>
2020-01-26 20:34:27 +0530

    changed route of py upload from / to /upload

--- ML/face/server_video.py
+++ ML/face/server_video.py
@@ -24,7 +24,7 @@ def upload_form():
 
 
 
-@app.route('/', methods=['POST'])
+@app.route('/upload', methods=['POST'])
 def upload_file():
     if request.method == 'POST':
         # check if the post request has the file part
